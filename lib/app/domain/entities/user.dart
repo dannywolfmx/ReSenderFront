fe28fc2c1cd914db0d3065664823e21da35e002a
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-10-10 12:20:01 -0500

    Se agrega parametros para el equatable

--- lib/app/domain/entities/user.dart
+++ lib/app/domain/entities/user.dart
@@ -1,13 +1,16 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:myapp/app/domain/entities/purcharse_order.dart';
 
 class User extends Equatable {
   final String username;
+  final List<PurcharseOrder> purcharseOrders;
 
   User({
     @required this.username,
+    this.purcharseOrders = const <PurcharseOrder>[],
   });
 
   @override
-  List<Object> get props => [username];
+  List<Object> get props => [username, purcharseOrders];
 }
